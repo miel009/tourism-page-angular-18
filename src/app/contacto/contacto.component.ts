51d18a29674749b55bdd9b58561ebbb019e5d972
miel009 <melanyascencio908@gmail.com>
2024-07-26 10:36:03 -0300

    ngOnInit - formularios y formas de validar datos

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,18 +6,48 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit{
  
   formularioContacto: FormGroup
+  usuarioActivo: any = {
+    nombre: 'Melany',
+    apellido: 'Ascencio',
+    dni: '51368679',
+  }
 
 
   constructor ( private form: FormBuilder){
-    // inicializamos el form
+    // inicializamos el form 
+    // validamos datos
     this.formularioContacto = this.form.group ({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
-      email:['', [Validators.required ,Validators.email] ]
+      apellido: [''],
+      dni: ['', [Validators.required, Validators.minLength(3)]],
+      email:['', [Validators.required ,Validators.email]]
     })
 
+  }
+
+  ngOnInit(): void {
+    // tambien se puede validar aca, depende de las validaciones necesarias
+   // this.formularioContacto.get('apellido')?.setValidators([Validators.required, Validators.minLength(3)])
+
+    // para borrar las validaciones anteriores: // en este caso para apellido
+    this.formularioContacto.get('apellido')?.clearValidators()
+    this.formularioContacto.get('apellido')?.updateValueAndValidity()
+    // setear por uno
+    //this.formularioContacto.get('nombre')?.setValue(this.usuarioActivo)
+
+    // setear en grupos: 
+    this.formularioContacto.patchValue({
+      nombre: this.usuarioActivo.nombre,
+      apellido: this.usuarioActivo.apellido,
+      dni: this.usuarioActivo.dni,
+    })
+    this.formularioContacto.get('nombre')?.disable()
+    // this.formularioContacto.get('apellido')?.disable()
+    this.formularioContacto.get('dni')?.disable()
+    //para setear nombre si es que ya se conoce el usuario.
   }
 
   hasErrors(_controlName: string , errorType: string){
